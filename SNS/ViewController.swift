07cbdc8954af70bb93f929216bdf16a8135c8010
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-25 21:02:15 +0900

    Create Activity View

--- SNS/ViewController.swift
+++ SNS/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
